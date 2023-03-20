f77af6e64b0b6f7872891fab3418a7a6e76e4148
dnonakolesax <domnakolesaxxx@gmail.com>
2023-03-20 21:26:53 +0300

    small fixes

--- lib/IMDbParser/include/IMDbParser.hpp
+++ lib/IMDbParser/include/IMDbParser.hpp
@@ -1,6 +1,9 @@
+#ifndef IMDBPARSER_HPP
+#define IMDBPARSER_HPP
 #include <vector>
 #include "utils.hpp"
 std::string getNconstByName (std::fstream& nameBasics, std::string name);
 std::vector<std::string> getDirectorFilms (std::fstream& titleCrew, std::string nconst);
 std::vector<Movie> getNotAdultMovies (std::fstream& titleBasics, std::vector<std::string> nconsts);
 std::vector<Movie> getRussianMoviesNames (std::fstream& titleAkas, std::vector<Movie> nconsts);
+#endif
